471629d200873d1d32f37e1dfe9a9599e558c6ef
= <=>
2021-11-02 20:09:18 +0200

    session 2 done

--- LabSession1/AdditiveCipher/src/App.java
+++ LabSession1/AdditiveCipher/src/App.java
@@ -6,11 +6,14 @@ public class App {
         // System.out.println(worker.decrypt(worker.encrypt("aba1c")));
         
         //Session 2
-         //MultiplicativeCipher Multiplicativeworker = new MultiplicativeCipher();
+        //  MultiplicativeCipher Multiplicativeworker = new MultiplicativeCipher();
         // //encrypting
         // //obtaining inverse of key to decrypt
         // System.out.println("should work " +worker2.inverseKey(26,5));
         // System.out.println("should not work " +worker2.inverseKey(2,26));
+        // System.out.println(Multiplicativeworker.encrypt("hello"));
+        // System.out.println(Multiplicativeworker.decrypt("vmiir"));
+
         AffineCipher affineWorker = new AffineCipher();
         System.out.println(affineWorker.encrypt("hel1lo"));
         System.out.println(affineWorker.decrypt("ypllu"));
